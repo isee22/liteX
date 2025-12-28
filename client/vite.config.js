addf8753b406dd3cba97a34c77e29b696486c5f3
isee <121077313@qq.com>
2025-12-29 02:55:15 +0800

    1

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,5 +2,13 @@ import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 
 export default defineConfig({
-  plugins: [uni()]
+  plugins: [uni()],
+  server: {
+    proxy: {
+      '/uploads': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
+  }
 })
